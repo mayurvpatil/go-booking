c556025a8146de0e60ed21ab523725ecd2734ded
mayurvpatil <mayurvpatil@live.com>
2021-12-12 01:20:45 +0530

    organised code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/mayurvpatil/go-server/pkg/handler"
 )
 
 const portNumber = ":8080"
 
 // main function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handler.Home)
+	http.HandleFunc("/about", handler.About)
 
 	fmt.Println("starting server on port number " + portNumber)
 	http.ListenAndServe(":8080", nil)
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,15 +1,17 @@
-package main
+package handler
 
 import (
 	"net/http"
+
+	"github.com/mayurvpatil/go-server/pkg/render"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.html")
 }
