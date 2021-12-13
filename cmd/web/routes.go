2099f6a5cce09827d0baf70ced778087058613a6
mayurvpatil <mayurvpatil@live.com>
2021-12-14 00:34:47 +0530

    go-chi as router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,11 +3,24 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/mayurvpatil/go-server/pkg/config"
 	"github.com/mayurvpatil/go-server/pkg/handler"
 )
 
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handler.Repo.Home)
+	mux.Get("/about", handler.Repo.About)
+
+	return mux
+}
+
+/* following function uses pat
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := pat.New()
@@ -18,3 +31,4 @@ func routes(app *config.AppConfig) http.Handler {
 	return mux
 
 }
+*/
